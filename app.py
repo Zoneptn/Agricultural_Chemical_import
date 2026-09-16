1a6f55fc9b6339fbba6e7bf4d0fd62c5b04070e0
Zone <pongthanat.b@gmail.com>
2026-09-16 10:38:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ st.set_page_config(page_title="Chemical Import Dashboard", layout="wide")
 
 overview_page = st.Page("views/overview.py", title="Chemical Overview", icon="🧪", default=True)
 comparison_page = st.Page("views/comparison_page.py", title="Chemical Comparison", icon="🔬")
+search_page = st.Page("views/registration_search.py", title="Registration Search", icon="🔎")
 
-nav = st.navigation([overview_page, comparison_page])
+nav = st.navigation([overview_page, comparison_page, search_page])
 nav.run()
